945e70eedf7b1b877e90ce60c2d4ccef590f0319
PickPlugins <32812881+pickplugins@users.noreply.github.com>
2020-02-16 12:49:53 +0600

    u

--- assets/admin/js/scripts.js
+++ assets/admin/js/scripts.js
@@ -6,55 +6,5 @@ jQuery(document).ready(function($)
 			
 			
 			
-		$(document).on('click', '.add_team_member_skill', function()
-			{
-				var skill_name = prompt("Skill name ?","");
-				
-				if(skill_name != null && skill_name != '')
-					{
-						$(".team-member-skills").append('<tr><td class="sorting"></td><td><input type="text" value="'+skill_name+'" name="team_member_skill['+skill_name+'][name]" placeholder="Programming" size="30"></td><td><input type="text" value="" name="team_member_skill['+skill_name+'][value]" placeholder="80%" size="30"></td><td><span class="remove-skill">X</span></td></tr>');
-					}
-
-		
-			})			
-			
-			
-		$(document).on('click', '.remove-skill', function()
-			{	
-				if (confirm('Do you really want to delete this field ?')) {
-					
-					$(this).parent().parent().remove();
-				}
-			})
-			
-			
-			
-			
-			
-		$(document).on('click', '.add_team_member_meta', function()
-			{
-				var meta_name = prompt("Meta name ?","");
-				
-				if(meta_name != null && meta_name != '')
-					{
-						$(".team-member-meta-fields").append('<tr><td class="sorting"></td><td><input type="text" value="'+meta_name+'" name="team_member_meta_fields['+meta_name+'][name]" placeholder="" size="30"></td><td><input type="text" value="" name="team_member_meta_fields['+meta_name+'][meta_key]" placeholder="meta_key" size="30"></td><td><span class="remove-meta">X</span></td></tr>');
-					}
-
-		
-			})				
-			
-			
-			
-		$(document).on('click', '.team-member-meta-fields .remove-meta', function()
-			{	
-				if (confirm('Do you really want to delete this field ?')) {
-					
-					$(this).parent().parent().remove();
-				}
-			})
-			
-			
-			
-			
 
 	});	
--- team.php
+++ team.php
@@ -15,174 +15,152 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 if ( ! defined('ABSPATH')) exit;  // if direct access 
 
+if( ! class_exists( 'Team' ) ) {
+    class Team{
 
-class Team{
-	
-	public function __construct(){
-	
-        define('team_plugin_url', plugins_url('/', __FILE__) );
-        define('team_plugin_dir', plugin_dir_path( __FILE__ ) );
-        define('team_plugin_name', 'Team' );
-        define('team_plugin_version', '1.22.0' );
+        public function __construct(){
 
-        include( 'includes/functions-data-upgrade.php' );
+            define('team_plugin_url', plugins_url('/', __FILE__));
+            define('team_plugin_dir', plugin_dir_path(__FILE__));
+            define('team_plugin_name', 'Team');
+            define('team_plugin_version', '1.22.0');
 
-        include( 'includes/class-post-meta-team.php' );
-        include( 'includes/class-post-meta-team-hook.php' );
+            include('includes/functions-data-upgrade.php');
 
-        include( 'includes/class-post-types.php' );
-        include( 'includes/class-settings.php' );
-        include( 'includes/class-functions.php' );
-        include( 'includes/class-shortcodes.php' );
+            include('includes/class-post-meta-team.php');
+            include('includes/class-post-meta-team-hook.php');
 
-        include( 'includes/class-settings-tabs.php' );
-        include( 'includes/functions-settings-hook.php' );
-        include( 'includes/functions-layout-hook.php' );
+            include('includes/class-post-types.php');
+            include('includes/class-settings.php');
+            include('includes/class-functions.php');
+            include('includes/class-shortcodes.php');
 
-        include( 'includes/class-post-meta-team-layout.php' );
-        include( 'includes/class-post-meta-team-layout-hook.php' );
-        include( 'includes/class-post-meta-team-member.php' );
-        include( 'includes/class-post-meta-team-member-hook.php' );
-        include( 'includes/class-admin-notices.php' );
+            include('includes/class-settings-tabs.php');
+            include('includes/functions-settings-hook.php');
+            include('includes/functions-layout-hook.php');
 
-
-        include_once team_plugin_dir.'/templates/team-showcase/team-showcase-hook.php';
+            include('includes/class-post-meta-team-layout.php');
+            include('includes/class-post-meta-team-layout-hook.php');
+            include('includes/class-post-meta-team-member.php');
+            include('includes/class-post-meta-team-member-hook.php');
+            include('includes/class-admin-notices.php');
 
 
-        include( 'templates/single-team/single-team_member-hook.php' );
-        include( 'includes/functions.php' );
+            include_once team_plugin_dir . '/templates/team-showcase/team-showcase-hook.php';
 
-        add_action( 'wp_enqueue_scripts', array( $this, 'team_front_scripts' ) );
-        add_action( 'admin_enqueue_scripts', array( $this, 'team_admin_scripts' ) );
-        add_action( 'plugins_loaded', array( $this, '_textdomain' ));
-        add_filter('widget_text', 'do_shortcode');
-        register_activation_hook( __FILE__, array( $this, '_activation' ) );
-        register_deactivation_hook(__FILE__, array( $this, '_deactivation' ));
-        add_filter( 'cron_schedules', array( $this, 'cron_recurrence_interval' ) );
 
+            include('templates/single-team/single-team_member-hook.php');
+            include('includes/functions.php');
+
+            add_action('wp_enqueue_scripts', array($this, '_front_scripts'));
+            add_action('admin_enqueue_scripts', array($this, '_admin_scripts'));
+            add_action('plugins_loaded', array($this, '_textdomain'));
+            add_filter('widget_text', 'do_shortcode');
+            register_activation_hook(__FILE__, array($this, '_activation'));
+            register_deactivation_hook(__FILE__, array($this, '_deactivation'));
+            add_filter('cron_schedules', array($this, 'cron_recurrence_interval'));
+
+
+        }
+
+
+        public function _textdomain(){
+
+            $locale = apply_filters('plugin_locale', get_locale(), 'team');
+            load_textdomain('team', WP_LANG_DIR . '/team/team-' . $locale . '.mo');
+
+            load_plugin_textdomain('team', false, plugin_basename(dirname(__FILE__)) . '/languages/');
+        }
+
+
+        public function _activation(){
+
+            // Reset permalink
+            $team_class_post_types = new team_class_post_types();
+            $team_class_post_types->_posttype_team_member();
+            flush_rewrite_rules();
+
+
+            do_action('team_activation');
+
+        }
+
+
+        public function team_uninstall(){
+
+            do_action('team_uninstall');
+        }
+
+        public function _deactivation(){
+
+            wp_clear_scheduled_hook('team_cron_upgrade_settings');
+            wp_clear_scheduled_hook('team_cron_upgrade_team_members');
+            wp_clear_scheduled_hook('team_cron_upgrade_team');
+
+            do_action('team_deactivation');
+        }
+
+
+        function cron_recurrence_interval($schedules){
+
+            $schedules['2minute'] = array(
+                'interval' => 120,
+                'display' => __('2 Minute', 'team')
+            );
+
+            $schedules['5minute'] = array(
+                'interval' => 300,
+                'display' => __('5 Minute', 'team')
+            );
+
+
+            return $schedules;
+        }
+
+
+        public function _front_scripts(){
+
+            wp_enqueue_script('jquery');
+
+            wp_register_style('single-team-member', plugins_url('assets/front/css/single-team-member.css', __FILE__));
+            wp_register_script('masonry', plugins_url('/assets/front/js/masonry.js', __FILE__), array('jquery'));
+            wp_register_script('imagesloaded', plugins_url('/assets/front/js/imagesloaded.js', __FILE__), array('jquery'));
+            wp_register_style('font-awesome-5', team_plugin_url . 'assets/admin/css/fontawesome.css');
+
+            do_action('team_front_scripts');
+        }
+
+        public function _admin_scripts(){
+
+            wp_enqueue_script('jquery');
+            wp_enqueue_script('jquery-ui-core');
+            wp_enqueue_script('jquery-ui-sortable');
+
+
+
+
+            wp_enqueue_style('wp-color-picker');
+            wp_enqueue_script('color-picker', plugins_url('/assets/admin/js/color-picker.js', __FILE__), array('wp-color-picker'), true, true);
+
+            wp_register_script('settings-tabs', team_plugin_url . 'assets/admin/js/settings-tabs.js', array('jquery'));
+            wp_register_style('settings-tabs', team_plugin_url . 'assets/admin/css/settings-tabs.css');
+            wp_register_style('font-awesome-5', team_plugin_url . 'assets/admin/css/fontawesome.css');
+
+            $cm_settings['codeEditor'] = wp_enqueue_code_editor(array('type' => 'text/css'));
+
+            wp_localize_script('jquery', 'cm_settings', $cm_settings);
+
+            wp_enqueue_script('wp-theme-plugin-editor');
+            //wp_enqueue_style('wp-codemirror');
+
+            do_action('team_admin_scripts');
+        }
 
     }
-		
-		
-	public function _textdomain() {
-
-        $locale = apply_filters( 'plugin_locale', get_locale(), 'team' );
-        load_textdomain('team', WP_LANG_DIR .'/team/team-'. $locale .'.mo' );
-
-        load_plugin_textdomain( 'team', false, plugin_basename( dirname( __FILE__ ) ) . '/languages/' );
-	}
-
-		
-	public function _activation(){
-
-
-		// Reset permalink
-		$team_class_post_types= new team_class_post_types();
-		$team_class_post_types->_posttype_team_member();
-		flush_rewrite_rules();
-
-
-        do_action( 'team_action_install' );
-		
-		}		
-
-
-	public function team_uninstall(){
-		
-		do_action( 'team_action_uninstall' );
-		}		
-		
-	public function _deactivation(){
-
-        wp_clear_scheduled_hook('team_cron_upgrade_settings');
-        wp_clear_scheduled_hook('team_cron_upgrade_team_members');
-        wp_clear_scheduled_hook('team_cron_upgrade_team');
-
-        do_action( 'team_action_deactivation' );
-		}
-
-
-
-
-    function cron_recurrence_interval( $schedules ){
-
-        $schedules['2minute'] = array(
-            'interval'  => 120,
-            'display'   => __( '2 Minute', 'team' )
-        );
-
-        $schedules['5minute'] = array(
-            'interval'  => 300,
-            'display'   => __( '5 Minute', 'team' )
-        );
-
-
-
-
-        return $schedules;
-    }
-
-
-
-
-
-
-
-    public function team_front_scripts(){
-			
-		wp_enqueue_script('jquery');
-
-        wp_register_style('single-team-member', plugins_url( 'assets/front/css/single-team-member.css', __FILE__ ));
-        wp_register_script('masonry', plugins_url( '/assets/front/js/masonry.js' , __FILE__ ) , array( 'jquery' ));
-        wp_register_script('imagesloaded', plugins_url( '/assets/front/js/imagesloaded.js' , __FILE__ ) , array( 'jquery' ));
-        wp_register_style('font-awesome-5', team_plugin_url.'assets/admin/css/fontawesome.css');
-
-
-
-		do_action('team_action_front_scripts');
-		}		
-		
-	public function team_admin_scripts(){
-		
-		wp_enqueue_script('jquery');
-		wp_enqueue_script('jquery-ui-core');
-		wp_enqueue_script('jquery-ui-sortable');
-		
-		wp_enqueue_script('team_admin_js', plugins_url( '/assets/admin/js/scripts.js' , __FILE__ ) , array( 'jquery' ));			
-		wp_localize_script('team_admin_js', 'team_admin_ajax', array( 'team_admin_ajaxurl' => admin_url( 'admin-ajax.php')));
-		
-		wp_localize_script( 'team_admin_js', 'L10n_team', array(
-							'confirm_text' => __( 'Confirm', 'team' ),
-							'done_text' => __( 'Done', 'team' ),
-							
-							));
-		
-
-		wp_enqueue_style( 'wp-color-picker' );
-		wp_enqueue_script( 'color-picker', plugins_url('/assets/admin/js/color-picker.js', __FILE__ ), array( 'wp-color-picker' ), true, true );
-
-        wp_register_script('settings-tabs', team_plugin_url.'assets/admin/js/settings-tabs.js', array( 'jquery' ));
-        wp_register_style('settings-tabs', team_plugin_url.'assets/admin/css/settings-tabs.css');
-        wp_register_style('font-awesome-5', team_plugin_url.'assets/admin/css/fontawesome.css');
-
-        $cm_settings['codeEditor'] = wp_enqueue_code_editor(array('type' => 'text/css'));
-
-        wp_localize_script('jquery', 'cm_settings', $cm_settings);
-
-        wp_enqueue_script('wp-theme-plugin-editor');
-        //wp_enqueue_style('wp-codemirror');
-
-		do_action('team_action_admin_scripts');
-		}		
-		
-
-
-
-
-
-	}
+}
 	
-	new Team();
+
+new Team();
 	
 
 	
